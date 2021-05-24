fe05724f85541a177e3342325f842cbac3251362
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-24 15:31:21 +0800

    Add a lot of things

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ var cors = require('cors')
 var querystring = require('querystring')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+const path = require('path')
+const serveStatic = require('serve-static')
 
 var client_id = process.env.CLIENT_ID // Your client id
 var client_secret = process.env.CLIENT_SECRET // Your secret
@@ -50,7 +52,13 @@ app.get('/login', function (req, res) {
   res.cookie(stateKey, state)
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email ugc-image-upload playlist-modify-public playlist-modify-private'
+  var scope = `
+    user-read-private
+    user-read-email
+    ugc-image-upload
+    playlist-modify-public
+    playlist-modify-private
+  `
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
@@ -71,7 +79,7 @@ app.get('/callback', function (req, res) {
   var storedState = req.cookies ? req.cookies[stateKey] : null
 
   if (state === null || state !== storedState) {
-    res.redirect('http://localhost:8080/?' +
+    res.redirect('/?' +
       querystring.stringify({
         error: 'state_mismatch'
       }))
@@ -97,13 +105,13 @@ app.get('/callback', function (req, res) {
           refresh_token = body.refresh_token
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:8080/?' +
+        res.redirect('/?' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
           }))
       } else {
-        res.redirect('http://localhost:8080/?' +
+        res.redirect('/?' +
           querystring.stringify({
             error: 'invalid_token'
           }))
@@ -159,7 +167,12 @@ app.put('/change_image', function (req, res) {
     }
   })
 
+})
 
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get('/.*/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
 console.log('Listening on 8888')
