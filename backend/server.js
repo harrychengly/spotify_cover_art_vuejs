2dd1cf9d4cac3fac8f6899735c7c2b21944837d5
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-24 16:05:48 +0800

    Do not import dotenv when in production

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'prodution') require('dotenv').config()
 
 var express = require('express') // Express web server framework
 var request = require('request') // "Request" library
